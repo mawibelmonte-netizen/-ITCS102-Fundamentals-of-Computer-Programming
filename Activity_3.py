2c5f7d9e0af2d1a220e2d6af903eea074f8395b8
mawi <mawibelmonte@gmail.com>
2026-08-19 23:22:22 +0800

    Add files via upload

---
+++ Activity_3.py
@@ -0,0 +1,59 @@
+#Strings 
+first_name = "Marwin" 
+middle_initial = "G"
+last_name = "Belmonte"
+favorite_color = "Black"
+
+print(
+	"My name is " + first_name + ".",
+	"My middle initial is " + middle_initial + ".",
+	"My last name is " + last_name + ".",
+	"My favorite color is " + favorite_color + ".",
+)
+
+#Integers 
+age = 18
+birth_year = 2007 
+grad_year = 2026
+friends = 10 
+
+print(
+	"My age is " + str(age) + ".",
+	"I was born in the year " + str(birth_year) + ".",
+	"I graduated senior high school in " + str(grad_year) + ".",
+	"I have" + str(friends) + "friends" + "."	
+)
+
+
+
+
+#Floats
+height = 5.6
+weight = 71.5 
+gen_ave = 94.44
+pocket_money = 2.25
+
+print(
+	"My height is " + str(height) + ".",	
+	"My weight is " + str(weight) + ".",	
+	"I graduated Senior High School with a General Average of " + str(gen_ave) + ".",
+	"All that's left in my wallet is " +str(pocket_money) + "pesos" + "."
+)
+
+
+
+#Booleans
+Distracted = True
+Kind = False
+Extravagant = True
+Fat = False
+
+print(
+	"Is Mawi Distracted?" + str(Distracted) + ".",
+	"Is he Kind?" + str(Kind) + ".",
+	"Am I extravagant?" + str(Extravagant) + ".",
+	"Do I look Fat?" + str(Fat) + ".",
+
+
+
+)
