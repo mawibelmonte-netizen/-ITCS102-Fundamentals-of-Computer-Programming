8b6c8ecdb096ad4ea8e5389f27f1b6a08e5f986b
mawi <mawibelmonte@gmail.com>
2026-08-26 00:42:34 +0800

    Add files via upload

---
+++ Code_challenge.py
@@ -0,0 +1,13 @@
+#escape sequence -> to modify the behavior of linear formatted string
+
+#\n - new line
+#\t - tab space
+#\r - carriage return / deprecated (obsolete)
+#\b - backspace
+#\\ - insert slash
+#\" - insert double quotation
+
+print(
+	"\t\t\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t**\t\t**\n\t\t\t\t\t\t\t\t***\t\t\t\t***\n\t\t\t\t\t\t\t****\t\t\t\t\t\t****\n\t\t\t\t\t\t*****\t\t\t\t\t\t\t\t*****\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\"Hi! My name is Marwin Belmonte,\t\t\t**\n\t\t\t\t\t**\t\t\t\tI'm from BSIT-1B\"\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**********************************************************************************\n\t\t\t\t\t**********************************************************************************"
+)
+			
