d595fd7c73a3ae1c687e21bc52f5af96d51c2b86
mawi <mawibelmonte@gmail.com>
2026-09-06 20:31:53 +0800

    Add files via upload

---
+++ Activity_7.py
@@ -0,0 +1,35 @@
+total = 5
+
+a = eval(input("ENTER ANY NUMBER ---->"))
+total += a
+
+b = eval(input("MORE ---->"))
+total -= b
+
+c = eval(input("MORE ---->"))
+total *= c
+
+d = eval(input("MORE ---->"))
+total -= d
+
+e = eval(input("MORE ---->"))
+total += e
+
+f = eval(input("MORE ---->"))
+total += f
+
+g = eval(input("MORE ---->"))
+total -= g
+
+h = eval(input("MORE ---->"))
+total += h
+
+i = eval(input("MORE ---->"))
+total -= i
+
+j = eval(input("OKAY THAT IS ENOUGH ---->"))
+total += j
+
+print(type(total))
+
+print("Total is", total)
---
+++ Activity_8.py
@@ -0,0 +1,15 @@
+hobbies = ""
+
+a = input("My Hobbies is -->")
+hobbies += a + ", "
+
+a = input("What else --->")
+hobbies += a + ", "
+
+a = input("What else --->")
+hobbies += a + ", "
+
+a = input("What else --->")
+hobbies += a + ", "
+
+print("my hobbies are {", hobbies ,"}")
