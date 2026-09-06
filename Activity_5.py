af90d9e9f867a4e40c10e3bb582206bd3e48d2b7
mawi <mawibelmonte@gmail.com>
2026-09-06 21:15:06 +0800

    Add files via upload

---
+++ Activity_5.py
@@ -0,0 +1,33 @@
+#operators activity 
+
+a = 7
+b = 5
+sum = a + b
+diff = a - b
+product = a * b
+quotient = a/b
+exponent = a ** b
+floor_div = a//b
+modulo = a%b
+
+print(
+	"The sum of ",a," and ",b," is ",sum
+)
+print(
+	"The difference of ",a," and ",b," is ",diff
+)
+print(
+	"The product of ",a," and ",b," is ",product
+)
+print(
+	"The quotient of ",a," and ",b," is ",quotient
+)
+print(
+	"The exponential response of ",a," and ",b," is ",exponent
+)
+print(
+	"The quotient of ",a," and ",b," is ",floor_div
+)
+print(
+	"The modulus of ",a," and ",b," is ",modulo
+)
