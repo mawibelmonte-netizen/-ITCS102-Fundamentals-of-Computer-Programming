89390a2ebac0bb03e2125803ca283b0eca9d72b6
mawi <mawibelmonte@gmail.com>
2026-09-09 11:23:51 +0800

    Add files via upload

---
+++ Activity_9.py
@@ -0,0 +1,27 @@
+x = 5
+y = 5 
+
+password1 = "sample"
+password2 = "sample" 
+
+#1>
+print(x > y)
+
+#2
+print(x < y)
+
+#3
+print(x >= y)
+
+#4
+print(x <= y)
+
+#5
+print(x == y)
+
+#6
+print(password1 == password2)
+
+#7
+print(password1 != password2)
+
