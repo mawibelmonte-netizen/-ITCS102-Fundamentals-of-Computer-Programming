583980e9843b560a02f2e12243e0f784b1c3632a
mawi <mawibelmonte@gmail.com>
2026-09-10 22:00:41 +0800

    Add files via upload

---
+++ Code_challenge2.py
@@ -0,0 +1,38 @@
+amount = eval(input(" Enter amount to deposit :"))
+print("Here's the breakdown of the amount :")
+n1 = 1000
+n2 = 500
+n3 = 200
+n4 = 100
+n5 = 50
+n6 = 20
+n7 = 10
+n8 = 5
+n9 = 1
+
+print(amount // n1, "-", n1)
+amount = amount % n1
+
+print(amount // n2, "-", n2)
+amount = amount % n2
+
+print(amount // n3, "-", n3)
+amount = amount % n3
+
+print(amount // n4, "-", n4)
+amount = amount % n4
+
+print(amount // n5, "-", n5)
+amount = amount % n5
+
+print(amount // n6, "-", n6)
+amount = amount % n6
+
+print(amount // n7, "-", n7)
+amount = amount % n7
+
+print(amount // n8, "-", n8)
+amount = amount % n8 
+
+print(amount // n9, "-", n9)
+amount = amount % n9
